6841529fdf6e52627347c7430b98fc31d22ec02f
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-03 12:40:36 -0400

    getting started on apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,24 @@
-const db = require("../db/db.json");
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app) {
+let savedNote = [];
 
-    
+fs.readFile("../db/db.json", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+});
+
+editNote = () => {
+    fs.writeFile("../db/db.json", JSON.stringify(savedNote), (err) => {
+        if (err) throw err;
+    });
+}
+
+
+
+module.exports = function (app) {
+
+    app.get("/api/notes", (req, res) => {
+        res.json(savedNote);
+      });
 }
