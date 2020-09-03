84e0f6849269c5bef0e692eaa15ae72cf5925362
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-03 11:18:10 -0400

    Created htmlroutes.js and got it working

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// Dependencies
 const express = require("express");
 
+// Creating "express" server and setting inital port
 const app = express();
 const PORT = process.env.PORT  || 6060
 
+// Sets up express app to handle parsing of data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
+require("./routes/htmlRoutes")(app);
+
+
+//Listener
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
